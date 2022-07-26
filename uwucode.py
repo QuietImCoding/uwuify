1acd4b5ba6d7ed5f0446a20a86be2dce118ae533
quietimcoding <dmonte3210@gmail.com>
2022-07-25 20:41:13 -0700

    uwu

---
+++ uwucode.py
@@ -0,0 +1,47 @@
+import sys
+
+intext = sys.stdin.read()
+
+primemap = { 3: ' ', 5: '.', 7: '!', 11: '?'}
+uwumap = { 0: 'u', 1: 'w', 2: 'U', 3: 'W' }
+
+def flipdict(d):
+    # thanks stack overflowz
+    return {value: key for key, value in d.items()}
+
+def uwuify(instr):
+    print('uwuifying.......')
+    ostr = ''
+    for b in instr:
+        tb = ord(b)
+        ctext = ''
+        for prime in primemap:
+            cchar = primemap[prime]
+            while tb % prime == 0:
+                ctext += cchar
+                tb //= prime
+        while tb // 4 > 0:
+            ctext += uwumap[tb % 4]
+            tb //= 4
+        ctext += uwumap[tb % 4]
+        ostr += ctext[::-1] + ','
+    return(ostr)
+
+def deuwu(instr):
+    print('deuwuifying.......')
+    ostr = ''
+    deuwumap = flipdict(uwumap)
+    deprimemap = flipdict(primemap)
+    for b in [k for k in instr.split(',') if k != '']:
+        val = 0
+        for letter in b:
+            if letter not in primemap.values():
+                val = val * 4 + deuwumap[letter]
+            else:
+                val *= deprimemap[letter]
+        ostr += chr(val)
+    return(ostr)
+
+uwued = uwuify(intext)
+print(uwued)
+print(deuwu(uwued))
